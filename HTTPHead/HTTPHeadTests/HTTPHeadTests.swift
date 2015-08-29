9e52c748ba463b3635518893772dd209d4480905
Francis Chong <francis@ignition.hk>
2015-08-29 16:20:22 +0800

    update tests

--- HTTPHead/HTTPHeadTests/HTTPHeadTests.swift
+++ HTTPHead/HTTPHeadTests/HTTPHeadTests.swift
@@ -14,27 +14,8 @@ class HTTPHeadTests: XCTestCase, NSURLSessionTaskDelegate {
 
     override func setUp() {
         let config = NSURLSessionConfiguration.defaultSessionConfiguration()
-        config.timeoutIntervalForRequest = 10.0
+        config.timeoutIntervalForRequest = 5.0
         session = NSURLSession(configuration: config, delegate: self, delegateQueue: NSOperationQueue.mainQueue())
-    }
-    
-    func testHead() {
-        let expectation = self.expectationWithDescription("complete")
-        let request = NSMutableURLRequest(URL: NSURL(string: "http://bit.ly/1Ub9VBf")!)
-        request.HTTPMethod = "HEAD"
-
-        var error : NSError? = nil
-        var response : NSURLResponse? = nil
-        session.dataTaskWithRequest(request) { (data, _response, _error) -> Void in
-            error = _error
-            response = _response
-            expectation.fulfill()
-        }.resume()
-
-        self.waitForExpectationsWithTimeout(60.0) { (_) -> Void in
-            XCTAssertNil(error)
-            XCTAssertNotNil(response)
-        }
     }
     
     func testGet() {
@@ -48,14 +29,15 @@ class HTTPHeadTests: XCTestCase, NSURLSessionTaskDelegate {
             error = _error
             response = _response
             expectation.fulfill()
-        }.resume()
+            }.resume()
         
-        self.waitForExpectationsWithTimeout(60.0) { (_) -> Void in
+        self.waitForExpectationsWithTimeout(10.0) { (_) -> Void in
             XCTAssertNil(error)
             XCTAssertNotNil(response)
         }
     }
     
+    /// Comment below will make the test pass
     func URLSession(session: NSURLSession, task: NSURLSessionTask, willPerformHTTPRedirection response: NSHTTPURLResponse, newRequest request: NSURLRequest, completionHandler: (NSURLRequest?) -> Void) {
         // disable redirect
         completionHandler(nil)
